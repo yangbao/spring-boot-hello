660ad056f10dc0e1d3204a08bc15a9b41626f50b
paul.yong <paul.yong@thomsonreuters.com>
2018-01-02 14:08:50 +0800

    hot boot support

--- src/main/java/com/reuters/App.java
+++ src/main/java/com/reuters/App.java
@@ -1,15 +1,45 @@
 package com.reuters;
 
+import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+import com.alibaba.fastjson.serializer.SerializerFeature;
+import com.alibaba.fastjson.support.config.FastJsonConfig;
+import com.alibaba.fastjson.support.spring.FastJsonHttpMessageConverter;
 
 /**
  * Hello world!
  * 启动类, @SpringBootApplication 指定这是一个spring boot的应用程序
  */
 @SpringBootApplication
-public class App 
+public class App extends WebMvcConfigurerAdapter
 {
+	
+	@Override
+	public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+    	super.configureMessageConverters(converters);
+		//1.定义一个convert 转换消息的对象，
+        FastJsonHttpMessageConverter fastConverter = new FastJsonHttpMessageConverter();
+
+        //2. 添加fastJson的配置信息，比如是否格式化返回json数据
+        FastJsonConfig fastJsonConfig = new FastJsonConfig();
+        fastJsonConfig.setSerializerFeatures(
+                SerializerFeature.PrettyFormat
+        );
+        //3.convert中添加配置信息
+        //4. 将convert添加到converts当中
+        fastConverter.setFastJsonConfig(fastJsonConfig);
+		
+    	converters.add(fastConverter);
+	}
+
+	
+	
+	
     public static void main( String[] args )
     {
         System.out.println( "Hello World!  开始启动" );
--- src/main/java/com/reuters/HelloController.java
+++ src/main/java/com/reuters/HelloController.java
@@ -17,12 +17,16 @@ public class HelloController {
 	public String hello(){
 		return "hello";
 	}
+	@RequestMapping("/hello2")
+	public String hello2(){
+		return "hello2";
+	}
 	//默认使用json返回，Spring boot 默认使用的json解析框架是jackson
 	@RequestMapping("/getDemo")
 	public Demo getDemo(){
 		Demo demo = new Demo();
 		demo.setId(1001);
-		demo.setName("TOm");
+		demo.setName("Tom");
 		return demo;
 	}
 }
